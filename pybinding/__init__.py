4b7022444591bed94eb87c1a3ae16cdf97e49d0b
Bert Jorissen <info@bertjorissen.be>
2024-06-27 22:22:55 +0200

    development

--- pybinding/__init__.py
+++ pybinding/__init__.py
@@ -1,7 +1,7 @@
 """Package for numerical tight-binding calculations in solid state physics"""
 __author__ = "Dean Moldovan"
 __copyright__ = "2015-2024, " + __author__
-__version__ = "1.0.3a"
+__version__ = "1.0.5a"
 import os
 import sys
 if sys.platform.startswith("linux"):
